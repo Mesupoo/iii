cb0e1330de4678c8c17551d12f2a14909008efb6
Mesupoo <1319303161@qq.com>
2017-01-17 17:55:20 +0800

    Revert "中华美食"
    This reverts commit 418837c37da7891cbb9ab1a3f04caf4fc557d882.

--- china_food/js/food_details.js
+++
@@ -1,6 +0,0 @@
-//接受location.href传来的数据
-$('#foot').load('php/footer.php');
-var thisurl = document.URL;
-var val = thisurl.split('?')[1];
-//eval()函数可将字符串"gongbaojiding"转化为变量gongbaojiding；
-$('.details').html(eval(val));
